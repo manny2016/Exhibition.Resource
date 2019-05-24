b7dc8e3f20b15e3552263fb344fd8e18052a536e
mannyq <Manny.Qiao@hotmail.com>
2019-05-24 16:59:59 +0800

    [Manny]

--- Exhibition.Portal.Web/src/app/resource/resource.component.ts
+++ Exhibition.Portal.Web/src/app/resource/resource.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Pipe } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse ,HttpRequest, HttpEvent} from '@angular/common/http';
 import{ManagementService} from '../services/ManagementService'
 import { Resource } from 'app/models/Resource';
+import { Subscription } from 'rxjs'
 declare var $: any;
 class DataTablesResponse {
   data: any[];
@@ -18,38 +19,52 @@ class DataTablesResponse {
 export class ResourceComponent implements OnInit {
   public dtOptions: DataTables.Settings = {};
   public resources:Resource[];
-  constructor(private service:ManagementService) { }  
+  accept = '*'
+  files:File[] = []
+  progress:number
+  url = 'https://evening-anchorage-3159.herokuapp.com/api/'
+  hasBaseDropZoneOver:boolean = false
+  httpEmitter:Subscription
+  httpEvent:HttpEvent<{}>
+  lastFileAt:Date
+
+  sendableFormData:FormData//populated via ngfFormData directive
+
+  dragFiles:any
+  validComboDrag:any
+  lastInvalids:any
+  fileDropDisabled:any
+  maxSize:any
+  baseDropValid:any
+
+  
+  constructor(public HttpClient:HttpClient,public service:ManagementService) { }  
   ngOnInit() {
     const that = this;
-    that.dtOptions = {
-        pagingType: 'simple',
-        pageLength: 2,
-        serverSide: true,
-        processing: true,
-        ajax: (dataTablesParameters: any, callback) => {
-            console.log("dataTablesParameters",dataTablesParameters);
-            that.service.GetResources().toPromise().then(data=>{
-                that.resources=data;
-            })
-            .catch(error=>{
-
-            })
-        //   that.http
-        //     .post<DataTablesResponse>(
-        //       'https://angular-datatables-demo-server.herokuapp.com/',
-        //       dataTablesParameters, {}
-        //     ).subscribe(resp => {
-        //       that.persons = resp.data;
-  
-        //       callback({
-        //         recordsTotal: resp.recordsTotal,
-        //         recordsFiltered: resp.recordsFiltered,
-        //         data: []
-        //       });
-        //     });
-        },
-        columns: [{ data: 'id' }, { data: 'type' }, { data: 'name' },{data:'fullName'}]
-      };
+    that.service.GetResources().toPromise().then(data=>{
+      that.resources=data;
+  })
+  // .catch(error=>{
+  // })
+  //   that.dtOptions = {
+  //       paging:false,        
+  //       serverSide: true,
+  //       processing: true,
+  //       autoWidth:true,
+  //       ajax: (dataTablesParameters: any, callback) => {
+  //           console.log("dataTablesParameters",dataTablesParameters);
+            
+  //       },
+  //       columns: [ { data: 'name' },{data:'fullName'}]
+  //     };
   }
-
+  public getIcoName(type:string){
+    console.log(type);
+    var ico :string = "description";
+    type = type.toLowerCase();
+    if(type=="images") return "folder_open";
+    if(type=="video")return "movie";
+    if(type=="h5") return "open_in_browser";    
+    return ico;
+  }
 }
